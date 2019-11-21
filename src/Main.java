e849552d9e39a8cdb9b795a23290f85b1556595d
Alexander Hjelm <nick.nertova@gmail.com>
2019-11-21 13:40:12 +0100

    Mouse listeners

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,14 @@ import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 import org.neuroph.nnet.Perceptron;
 
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         // Swing example
 
@@ -17,6 +21,47 @@ public class Main {
         DisplayGraphics dg = new DisplayGraphics();
         dg.AddRectangle(0, 0, 100, 100);
         dg.AddRectangle(100, 100, 50, 50);
+
+        dg.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // Dummy
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("Mouse pressed");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println("Mouse released");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                // Dummy
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                // Dummy
+            }
+        });
+
+        dg.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println(x + "," + y);//these co-ords are relative to the component
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                // Dummy
+            }
+        });
 
         f.add(dg);
 
@@ -45,5 +90,8 @@ public class Main {
         neuralNetwork.learn(trainingSet);
         // save the trained network into file
         neuralNetwork.save("or_perceptron.nnet");
+
+        System.out.println("Initialization done");
+
     }
 }
